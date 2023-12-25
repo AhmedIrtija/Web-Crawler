6bb4555c5b12d9ee7f0b627b493983c9f42037cc
AhmedIrtija <airtija@ucdavis.edu>
2023-12-24 16:56:46 -0800

    part 1

---
+++ Part1.py
@@ -0,0 +1,24 @@
+import socket 
+import struct
+import time
+
+
+tmz = "tmz.com"
+
+root_records = []
+#All the root records from the provided website from a - m
+root_records.append("198.41.0.4")
+root_records.append("199.9.14.201")
+root_records.append("192.33.4.12")
+root_records.append("199.7.91.13")
+root_records.append("192.203.230.10")
+root_records.append("192.5.5.241")
+root_records.append("192.112.36.4")
+root_records.append("198.97.190.53")
+root_records.append("192.36.148.17")
+root_records.append("192.58.128.30")
+root_records.append("193.0.14.129")
+root_records.append("199.7.83.42")
+root_records.append("202.12.27.33")
+
+
